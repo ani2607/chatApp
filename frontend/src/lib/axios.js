42b8899d3e8a51fe4798bdb3e3fcc9a6a05fd29b
aniket sharma <aniket220603@gmail.com>
2024-12-20 00:47:01 +0530

    updated_cookie_logic

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `${import.meta.env.VITE_BACKEND_URI}/api`,
+  baseURL: import.meta.env.VITE_REACT_APP_MODE === "development" ? "http://localhost:5001/api" : `${import.meta.env.VITE_REACT_APP_BACKEND_URI}/api`,
   withCredentials: true,
 });
